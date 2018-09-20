373ac3ed98860f49432af698a32937336ec5b8bd
Ludswing Cahuec <ludswingmichael@outlook.com>
2018-09-19 19:45:13 -0600

    Agregar archivos de proyecto.

---
+++ ExaP9/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExaP9.Startup))]
+namespace ExaP9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
